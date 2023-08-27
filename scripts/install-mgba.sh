b575183b0bec4139546a154cd2fcbe7e7b4877d7
Josh.5 <jsunnex@gmail.com>
2023-08-28 11:12:34 +1200

    Add some default config for mGBA

--- scripts/install-mgba.sh
+++ scripts/install-mgba.sh
@@ -5,7 +5,7 @@
 # File Created: Sunday, 27th August 2023 8:28:04 am
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Sunday, 27th August 2023 10:52:09 am
+# Last Modified: Monday, 28th August 2023 11:10:59 am
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 
@@ -50,9 +50,124 @@ else
 fi
 
 
-# Configure EmulationStation DE
+# Generate mGBA Emulation directory structure
 romsPath="/mnt/games/Emulation/roms"
-mkdir -p "${romsPath:?}/gba"
+biosPath="/mnt/games/Emulation/bios"
+savesPath="/mnt/games/Emulation/saves"
+storagePath="/mnt/games/Emulation/storage"
+mkdir -p \
+    "${USER_HOME:?}"/.config/mgba \
+    "${savesPath:?}"/mgba/states \
+    "${savesPath:?}"/mgba/saves \
+    "${storagePath:?}"/mgba/cheats \
+    "${storagePath:?}"/mgba/screenshots \
+    "${storagePath:?}"/mgba/patches \
+    "${romsPath:?}"/gba
+
+# Generate a default config if missing
+if [ ! -f "${USER_HOME:?}/.config/mgba/config.ini" ]; then
+    cat << EOF > "${USER_HOME:?}/.config/mgba/config.ini"
+[gba.input.SDLB]
+keyRight=-1
+keyDown=-1
+axisUpValue=-12288
+hat0Up=6
+tiltAxisY=3
+device0=030000005e0400008e02000010010000
+axisLeftAxis=-0
+gyroAxisZ=-1
+gyroSensitivity=2.2e+09
+gyroAxisX=0
+axisLeftValue=-12288
+axisRightAxis=+0
+keyR=5
+keyL=4
+hat0Right=4
+hat0Left=5
+tiltAxisX=2
+keyB=1
+keyUp=-1
+gyroAxisY=1
+keySelect=6
+keyLeft=-1
+axisRightValue=12288
+keyA=0
+hat0Down=7
+axisDownValue=12288
+keyStart=7
+axisUpAxis=-1
+axisDownAxis=+1
+
+[gba.input-profile.Xbox 360 Controller]
+keyRight=-1
+keyDown=-1
+axisUpValue=-12288
+hat0Up=6
+axisLeftAxis=-0
+axisLeftValue=-12288
+axisRightAxis=+0
+keyR=5
+keyL=4
+hat0Right=4
+hat0Left=5
+keyB=1
+keyUp=-1
+keySelect=6
+keyLeft=-1
+axisRightValue=12288
+keyA=0
+hat0Down=7
+axisDownValue=12288
+keyStart=7
+axisUpAxis=-1
+axisDownAxis=+1
+
+[ports.qt]
+width=863
+height=641
+gb.pal[0]=8953928
+gb.pal[1]=4745264
+gb.pal[2]=2637856
+gb.pal[3]=1583112
+gb.pal[4]=8953928
+gb.pal[5]=4745264
+gb.pal[6]=2637856
+gb.pal[7]=1583112
+gb.pal[8]=8953928
+gb.pal[9]=4745264
+gb.pal[10]=2637856
+gb.pal[11]=1583112
+hwaccelVideo=1
+rewindBufferCapacity=1000
+savestatePath=${savesPath:?}/mgba/states
+cheatsPath=${storagePath:?}/mgba/cheats
+screenshotPath=${storagePath:?}/mgba/screenshots
+savegamePath=${savesPath:?}/mgba/saves
+patchPath=${storagePath:?}/mgba/patches
+showLibrary=1
+videoScale=5
+updateAutoCheck=0
+rewindEnable=1
+fpsTarget=59.72750056960583
+libraryStyle=1
+
+EOF
+fi
+if [ ! -f "${USER_HOME:?}/.config/mgba/qt.ini" ]; then
+    cat << EOF > "${USER_HOME:?}/.config/mgba/qt.ini"
+[shortcutButton]
+quit=2
+
+[shortcutProfileAxis.Xbox%20360%20Controller]
+quit=@String(\0-1)
+
+[shortcutProfileButton.Xbox%20360%20Controller]
+quit=2
+
+EOF
+fi
+
+# Configure EmulationStation DE
 cat << 'EOF' > "${romsPath:?}/gba/systeminfo.txt"
 System name:
 gba
@@ -85,5 +200,12 @@ if ! grep -ri "gba:" "${romsPath:?}/systems.txt" &>/dev/null; then
     chown -R ${PUID:?}:${PGID:?} "${romsPath:?}/systems.txt"
 fi
 sed -i 's|^gba:.*$|gba: Nintendo Game Boy Advance|' "${romsPath:?}/systems.txt"
+
+# Set correct ownership of created paths
+chown -R ${PUID:?}:${PGID:?} \
+    "${USER_HOME:?}"/.config/mgba \
+    "${savesPath:?}"/mgba \
+    "${storagePath:?}"/mgba \
+    "${romsPath:?}"/gba
 
 echo "DONE"
