c01eda9ff6b7a81327074e1ef4531da63b8da5c1
Josh.5 <jsunnex@gmail.com>
2023-09-16 10:18:31 +1200

    Some small improvements
    I want to read the steam library path from steam's config incase it is not set to `/mnt/games/SteamLibrary`

--- scripts/es_steam_import.sh
+++ scripts/es_steam_import.sh
@@ -5,7 +5,7 @@
 # File Created: Monday, 11th September 2023 3:57:47 pm
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Monday, 11th September 2023 8:08:55 pm
+# Last Modified: Saturday, 16th September 2023 10:17:17 am
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 #
@@ -45,23 +45,34 @@ EOF
 }
 
 romsPath="/mnt/games/Emulation/roms/steam"
-steamPath="/mnt/games/SteamLibrary/steamapps"
+steamPaths=$(cat "${USER_HOME:?}/.steam/steam/steamapps/libraryfolders.vdf" | grep "path" | awk -F"\"" '{print $4}')
 
 if [[ -d "${romsPath:?}" ]]; then
     rm -r "${romsPath:?}"
 fi
 mkdir -p "${romsPath:?}"
 
-__steamapps=$(ls "${steamPath}" | grep ".acf")
+for steamPath in ${steamPaths}; do
+    if [[ "${steamPath:?}" == *"${USER_HOME:?}"* ]]; then
+        echo "Ignoring steam library '${steamPath:?}'"
+        continue
+    fi
+    echo "Parsing steam library '${steamPath:?}'"
+    __steamapps=$(ls "${steamPath:?}/steamapps/" | grep ".acf")
 
-for __steamapp in ${__steamapps:?}; do
-    steam_id=$(grep "appid" "${steamPath:?}/${__steamapp}" | cut -d '"' -f 4)
-    steam_name_dirty=$(grep "name" "${steamPath:?}/${__steamapp}" | cut -d '"' -f 4)
-    steam_name=$(echo "${steam_name_dirty:?}" | sed -e 's/"//g')
-	if [[ ! -z ${steam_name:?} ]]; then
-        es_entry="${romsPath:?}/$(echo ${steam_name:?} | sed -e 's#/#_#g').desktop"
-        es_shortcut=$(CreateXDGDesktopShorcut ${steam_id:?} ${steam_name:?})
+    for __steamapp in ${__steamapps:?}; do
+        steam_id=$(grep "appid" "${steamPath:?}/steamapps/${__steamapp}" | cut -d '"' -f 4)
+        steam_name_dirty=$(grep "name" "${steamPath:?}/steamapps/${__steamapp}" | cut -d '"' -f 4)
+        steam_name=$(echo "${steam_name_dirty:?}" | sed -e 's/"//g')
+        print_step_header "Found steam app '${steam_name_dirty:?}'"
+        if [[ ! -z ${steam_name:?} ]]; then
+            print_step_header "Creating shortcut for steam app '${steam_name_dirty:?}'"
+            es_entry="${romsPath:?}/$(echo ${steam_name:?} | sed -e 's#/#_#g').desktop"
+            es_shortcut=$(CreateXDGDesktopShorcut ${steam_id:?} ${steam_name:?})
 
-        echo "${es_shortcut:?}" > "${es_entry:?}"
-	fi
+            echo "${es_shortcut:?}" > "${es_entry:?}"
+        else
+            print_step_header "Shortcut for steam app '${steam_name_dirty:?}' already exists"
+        fi
+    done
 done
