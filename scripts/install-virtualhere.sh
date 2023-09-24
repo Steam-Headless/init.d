93fba129a2c0326556311b2982ce810acb50e4b7
alansari <terminal.v@gmail.com>
2023-09-24 08:29:59 +0300

    Enable supervisor monitoring of virtualhere server

--- scripts/install-virtualhere.sh
+++ scripts/install-virtualhere.sh
@@ -26,25 +26,57 @@ set -euo pipefail
 source "${USER_HOME:?}/init.d/helpers/functions.sh"
 
 
-# Ensure this script is being executed as the default user
-exec_script_as_default_user
-
-
 # Config
 package_name="VirtualHere"
 package_description="The server-side component of VirtualHere"
-package_icon_url=""
-package_executable=""
+package_executable="${USER_HOME:?}/.local/bin/vhusbdx86_64"
 package_category="Utility"
 print_package_name
 
 
 # Check for a new version to install
-__latest_package_version=""
-__latest_package_id=""
 __latest_package_url="https://www.virtualhere.com/sites/default/files/usbserver/vhusbdx86_64"
-print_step_header "Latest ${package_name:?} version: ${__latest_package_version:?}"
+print_step_header "Latest ${package_name:?}"
 
+if ([ ! -f "${package_executable:?}" ] || [ ! -f "/tmp/.user-script-${package_name,,}-installed" ]); then
+    # Download Server
+    print_step_header "Downloading ${package_name:?}"
+    wget -O "${USER_HOME:?}/.local/bin/${package_name,,}" \
+        --quiet -o /dev/null \
+        --no-verbose --show-progress \
+        --progress=bar:force:noscroll \
+        "${__latest_package_url:?}"
+    chmod +x ${USER_HOME:?}/.local/bin/${package_name,,}
+	
+    # Mark this version as installed
+    touch "/tmp/.user-script-${package_name,,}-installed"
+else
+    print_step_header "${package_name:?} already installed"
+fi
 
+print_step_header "Enabeling ${package_name:?} supervisor unit"
+# Setup virtualhere supervised
+if [ ! -f "/etc/supervisor.d/virtualhere.ini" ]; then
+    cat << EOF > "/etc/supervisor.d/virtualhere.ini"
+[program:virtualhere]
+priority=99
+autostart=false
+autorestart=true
+startretries=50
+user=root
+directory=
+command=${USER_HOME:?}/.local/bin/${package_name,,}
+environment=
+stopsignal=INT
+stdout_logfile=/home/%(ENV_USER)s/.cache/log/virtualhere.log
+stdout_logfile_maxbytes=10MB
+stdout_logfile_backups=7
+stderr_logfile=/home/%(ENV_USER)s/.cache/log/virtualhere.err.log
+stderr_logfile_maxbytes=10MB
+stderr_logfile_backups=7
+EOF
+fi
+
+supervisorctl start virtualhere
 
 echo "DONE"
