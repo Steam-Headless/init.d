221965266f2f9b1b130cc4fdb4794109d41d4c85
alansari <terminal.v@gmail.com>
2023-10-10 11:43:09 +0300

    Script added to remove entries for testing

--- standalone/auto-sunset.sh
+++ standalone/auto-sunset.sh
@@ -1,3 +1,5 @@
 # Remove previously added entries
+sunshine_conf=${USER_HOME:?}/.config/sunshine/apps.json
+
 cat ${sunshine_conf} | jq 'del(.apps[] | select(.output == "SH-run.txt"))' > /tmp/sunshine.json 
 mv -f /tmp/sunshine.json ${sunshine_conf}
