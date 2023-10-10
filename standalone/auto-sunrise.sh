e474bd8bddf028c26679102a8cedf2579cf3c452
alansari <terminal.v@gmail.com>
2023-10-10 08:58:43 +0300

    Initial commit

---
+++ standalone/auto-sunrise.sh
@@ -0,0 +1,84 @@
+games=()
+steamgriddb_api="INSERTAPIKEYHERE"
+sunshine_conf=${USER_HOME:?}/.config/sunshine/apps.json
+sunshine_tmp=/tmp/apps.json
+
+function getCoverArt {
+    GAME_NAME="$1"
+
+    # Encode the game name for URL
+    GAME_NAME_ENCODED=$(printf %s "${GAME_NAME:?}" | jq -sRr @uri)
+
+    # Make a GET request to the SteamGridDB API to search for the game by name
+    RESPONSE=$(curl -H "Authorization: Bearer ${steamgriddb_api:?}" -s "https://www.steamgriddb.com/api/v2/search/autocomplete/$GAME_NAME_ENCODED")
+
+    # Parse the JSON response to get the game ID
+    GAME_ID=$(echo "$RESPONSE" | jq -r '.data[0].id')
+
+    # Check if the game ID is valid
+    if [ "$GAME_ID" == "null" ]; then
+      echo "Game not found: $GAME_NAME"
+      return 1
+    fi
+
+    # Make another GET request to the SteamGridDB API to get the poster art for the game by ID
+    RESPONSE=$(curl -H "Authorization: Bearer ${steamgriddb_api:?}" -s "https://www.steamgriddb.com/api/v2/grids/game/$GAME_ID?dimensions=600x900")
+
+    # Parse the JSON response to get the poster art URL
+    POSTER_URL=$(echo "$RESPONSE" | jq -r '.data[0].url')
+
+    # Check if the poster art URL is valid
+    if [ "$POSTER_URL" == "null" ]; then
+      echo "Poster art not found for game: $GAME_NAME"
+      return 2
+    fi
+
+    # Download the poster art image from the URL
+    curl -s -o "$GAME_NAME.jpg" "$POSTER_URL"
+
+    # Resize the image to fit the  dimensions
+    #convert "$GAME_NAME.jpg" -resize 300x450\! "$GAME_NAME.png"
+    convert "$GAME_NAME.jpg" "$GAME_NAME.png"
+    if [ -f "$GAME_NAME.jpg" ]; then
+      rm "$GAME_NAME.jpg"
+    fi
+}
+
+for manifest in /mnt/games/SteamLibrary/steamapps/appmanifest_*.acf; do
+  appid=$(basename "$manifest" | cut -d_ -f2 | cut -d. -f1)
+  name=$(grep -oP '"name"\s+"\K[^"]+' "$manifest")
+  games+=("$appid $name")
+done
+
+cp -v ${sunshine_conf:?} ${sunshine_tmp:?}
+
+for game in "${!games[@]}"
+  do
+    __game_name=$(echo ${games[$game]} | cut -d " " -f 2-)
+    __game_id=$(echo ${games[$game]} | cut -d " " -f 1)
+    __poster_path=${USER_HOME:?}/.local/share/posters/"${__game_name:?}".png
+    __game_run="/usr/bin/sunshine-run steam steam://rungameid/${__game_id:?}"
+
+    if [ -f "${__poster_path:?}" ]; then
+      echo "Found Poster for ${__game_name:?}"
+    else
+      echo "Downloading Poster for ${__game_name:?}"
+      getCoverArt "${__game_name:?}" \
+	&& mv "${__game_name:?}".png ${USER_HOME:?}/.local/share/posters/
+    fi
+
+    cat ${sunshine_tmp:?} | jq '.apps += [
+      { 
+        "name": "'"${__game_name:?}"'",
+        "output":"SH-run.txt",
+        "cmd": "",
+        "detached": "['"${__game_run:?}"']",
+        "exclude-global-prep-cmd": "false",
+        "elevated": "false",
+        "image-path": "'"${__poster_path:?}"'",
+        "working-dir": "/home/default"
+      } ]' > /tmp/sunshine.json
+    mv -f /tmp/sunshine.json ${sunshine_tmp:?}
+  done
+
+#cat ${sunshine_tmp:?}
