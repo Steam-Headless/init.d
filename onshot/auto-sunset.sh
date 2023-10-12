b4d8cd10ec5edb69ff8cb0454ad7dba67344952b
alansari <terminal.v@gmail.com>
2023-10-12 08:19:33 +0300

    Attempt at using Zenity

--- onshot/auto-sunset.sh
+++
@@ -1,5 +0,0 @@
-# Remove previously added entries
-sunshine_conf=${USER_HOME:?}/.config/sunshine/apps.json
-
-cat ${sunshine_conf} | jq 'del(.apps[] | select(.output == "SH-run.txt"))' > /tmp/sunshine.json 
-mv -f /tmp/sunshine.json ${sunshine_conf}
