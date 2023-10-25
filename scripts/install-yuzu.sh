d50092c38cd7da17fbd7457b73a538af939c1e79
Josh.5 <jsunnex@gmail.com>
2023-10-25 15:01:01 +1300

    Fix Yuzu installation scripts

--- scripts/install-yuzu.sh
+++ scripts/install-yuzu.sh
@@ -5,7 +5,7 @@
 # File Created: Wednesday, 23rd August 2023 7:16:02 pm
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Sunday, 17th September 2023 4:26:30 pm
+# Last Modified: Wednesday, 25th October 2023 2:56:30 pm
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 #
@@ -41,21 +41,18 @@ print_package_name
 
 # Check for a new version to install
 __registry_package_json=$(wget -O - -o /dev/null https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest)
-__latest_package_version=$(echo ${__registry_package_json:?} | jq -r ".assets[2] | .name" | cut -d "-" -f 3 | cut -d "." -f 1)
-__latest_package_id=$(echo ${__registry_package_json:?} | jq -r ".assets[2] | .name" | cut -d "-" -f 2,3)
+__latest_package_version=$(echo ${__registry_package_json:?} | jq -r '.assets[] | select(.name | test("\\.appimage$"; "i"))' | jq -r '.name' | cut -d "-" -f 3 | cut -d "." -f 1)
+__latest_package_id=$(echo ${__registry_package_json:?} | jq -r '.assets[] | select(.name | test("\\.appimage$"; "i"))' | jq -r '.id')
+__latest_package_url=$(echo ${__registry_package_json:?} | jq -r '.assets[] | select(.name | test("\\.appimage$"; "i"))' | jq -r '.browser_download_url')
 print_step_header "Latest ${package_name:?} version: ${__latest_package_version:?}"
 __installed_version=$(catalog -g ${package_name,,})
 
 
 # Only install if the latest version does not already exist locally
 if ([ ! -f "${package_executable:?}" ] || [ "${__installed_version:-X}" != "${__latest_package_version:?}" ]); then
-    # Fetch download links
-    print_step_header "Fetching download link for ${package_name:?} version ${__latest_package_version:?}"
-    __latest_url=$(wget -O - -o /dev/null https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest | jq -r ".assets[2] | .browser_download_url")
-
     # Download Appimage to Applications directory
     print_step_header "Downloading ${package_name:?} version ${__latest_package_version:?}"
-    fetch_appimage_and_make_executable "${__latest_url:?}"
+    fetch_appimage_and_make_executable "${__latest_package_url:?}"
 
     # Ensure this package has a start menu link (will create it if missing)
     print_step_header "Ensuring menu short is present for ${package_name:?}"
