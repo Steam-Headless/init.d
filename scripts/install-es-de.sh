0d29ee8c36bbf44a2d91460bfb25d2d64dc2a114
Josh.5 <jsunnex@gmail.com>
2025-05-12 20:42:05 +1200

    Fix location of download media to default ESDE location

--- scripts/install-es-de.sh
+++ scripts/install-es-de.sh
@@ -5,7 +5,7 @@
 # File Created: Wednesday, 23rd August 2023 7:16:02 pm
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Sunday, 17th September 2023 4:02:38 pm
+# Last Modified: Monday, 12th May 2025 8:41:12 pm
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 #
@@ -22,14 +22,11 @@
 
 set -euo pipefail
 
-
 # Import helpers
 source "${USER_HOME:?}/init.d/helpers/functions.sh"
 
-
 # Ensure this script is being executed as the default user
 exec_script_as_default_user
-
 
 # Config
 package_name="EmulationStation-DE"
@@ -39,13 +36,11 @@ package_executable="${USER_HOME:?}/.local/bin/${package_name:?}.AppImage"
 package_category="Game"
 print_package_name
 
-
 # Check for a new version to install
 __registry_package_json=$(wget -O - -o /dev/null https://gitlab.com/api/v4/projects/es-de%2Femulationstation-de/packages)
 __latest_package_version=$(echo ${__registry_package_json:?} | jq -c 'map(select(.name | contains("ES-DE_Stable")))' | jq -r '.[-1].version')
 __latest_package_id=$(echo ${__registry_package_json:?} | jq -c 'map(select(.name | contains("ES-DE_Stable")))' | jq -r '.[-1].id')
 print_step_header "Latest ${package_name:?} version: ${__latest_package_version:?}"
-
 
 # Only install if the latest version does not already exist locally
 if [ ! -f "${package_executable:?}" ] || [ ! -f "${USER_HOME:?}/.local/share/${package_name:?}/.installed-${package_name:?}-${__latest_package_version:?}" ]; then
@@ -65,16 +60,16 @@ if [ ! -f "${package_executable:?}" ] || [ ! -f "${USER_HOME:?}/.local/share/${p
     ensure_menu_shortcut
 
     # Mark this version as installed
-    mkdir -p  "${USER_HOME:?}/.local/share/${package_name:?}"
+    mkdir -p "${USER_HOME:?}/.local/share/${package_name:?}"
     touch "${USER_HOME:?}/.local/share/${package_name:?}/.installed-${package_name:?}-${__latest_package_version:?}"
 else
     print_step_header "Latest version of ${package_name:?} version ${__latest_package_version:?} already installed"
 fi
 
-
 # Generate EmulationStation directory structure
 romsPath="/mnt/games/Emulation/roms"
 toolsPath="/mnt/games/Emulation/tools"
+downloadedMediaPath="/mnt/games/Emulation/downloaded_media"
 if [ -d "${USER_HOME:?}"/.emulationstation ]; then
     mv "${USER_HOME:?}"/.emulationstation "${USER_HOME:?}"/ES-DE
 fi
@@ -82,14 +77,15 @@ fi
 mkdir -p \
     "${USER_HOME:?}"/ES-DE \
     "${romsPath:?}" \
-    "${toolsPath:?}"/downloaded_media
+    "${toolsPath:?}" \
+    "${downloadedMediaPath:?}"
 
 # Configure EmulationStation DE defaults
 if [ ! -f "${USER_HOME:?}/ES-DE/es_settings.xml" ]; then
-    cat << 'EOF' > "${USER_HOME:?}/ES-DE/es_settings.xml"
+    cat <<EOF >"${USER_HOME:?}/ES-DE/es_settings.xml"
 <?xml version="1.0"?>
-<string name="MediaDirectory" value="/mnt/games/Emulation/tools/downloaded_media" />
-<string name="ROMDirectory" value="/mnt/games/Emulation/roms/" />
+<string name="MediaDirectory" value="${downloadedMediaPath:?}" />
+<string name="ROMDirectory" value="${romsPath:?}/" />
 <string name="ScreensaverSlideshowImageDir" value="~/ES-DE/slideshow/custom_images" />
 EOF
 fi
